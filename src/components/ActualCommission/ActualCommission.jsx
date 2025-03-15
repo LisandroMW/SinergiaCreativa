2ceaa98df8c2991a0dc856143a211f15256b5bb8
LisandroMW <Lisandroweinzettel@gmail.com>
2025-03-15 17:07:33 -0300

    adding actualcommission

--- src/components/ActualCommission/ActualCommission.jsx
+++ src/components/ActualCommission/ActualCommission.jsx
@@ -1,12 +1,36 @@
-import "./ActualCommission.css"
+import React, { useState } from 'react';
+import "./ActualCommission.css";
 
-function ActualCommission(){
-    return(
-        <div className="container">
-            <label htmlFor="volume" className="label">Comisión Actual</label>
-            <input className="input" type="range" id="volume" min="0" max="100"/>
+function ActualCommission() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div className="range-input">
+      <div className="label-container">
+        <label htmlFor="commission-slider" className="text-container">
+          Comisión Actual
+        </label>
+        <div className="value">
+          <div>{value}%</div>
         </div>
-    )
+      </div>
+      <div className="value-container">
+        <input
+          id="commission-slider"
+          type="range"
+          min="0"
+          max="100"
+          value={value}
+          step="1"
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default ActualCommission
+export default ActualCommission;
--- src/components/Commissions/Commissions.jsx
+++ src/components/Commissions/Commissions.jsx
@@ -1,14 +1,25 @@
 import "./Commissions.css"
 import CustomInput from "../CustomInput/CustomInput"
 import MonthSelector from "../MonthSelect/MonthSelect"
+import ActualCommission from "../ActualCommission/ActualCommission"
 
 function Commissions(){
     return(
-        <div className="labels-inputs">
-            <CustomInput text="Nombre de Socio/a" id="partner-name" type="text"/>
-            <MonthSelector label="Mes" id="birthMonth" placeholder="Selecciona un mes"/>
-            <CustomInput text="Mi ticket Promedio en USD" id="average ticket" type="number"/>
-            <CustomInput text="Valor USD (Valor de cambio)" id="USD value" type="number"/>            
+        <div className="commissions">
+            <div className="labels-inputs">
+                <CustomInput text="Nombre de Socio/a" id="partner-name" type="text"/>
+                <MonthSelector label="Mes" id="birthMonth" placeholder="Selecciona un mes"/>
+                <CustomInput text="Mi ticket Promedio en USD" id="average ticket" type="number"/>
+                <CustomInput text="Valor USD (Valor de cambio)" id="USD value" type="number"/> 
+                
+            </div>
+            <div className="percentage-container">
+                <ActualCommission/>
+            </div>
+            <div className="ticket-container">
+                
+            </div>
+                
         </div>
         
     )
